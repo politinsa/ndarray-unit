f2f6fb503cc0df3e6df04cb2bdb338fb10232cc9
Etienne D <etiennedelahaye@outlook.com>
2019-12-02 00:13:03 +0000

    Add ndarray dependencies

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,11 @@
 use ndarray_unit::*;
 
+extern crate ndarray;
+
+use ndarray::Array;
+
 fn main() {
     println!("{}", &get_meter() / &get_second());
+    println!("{}", Array::linspace(0.0,10.0,11));
 }
 
